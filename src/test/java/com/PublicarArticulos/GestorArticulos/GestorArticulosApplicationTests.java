b55c7a02f7144f651312cff08508ee61f164ce3a
Sharon Rodriguez <sharo2599@gmail.com>
2025-09-05 00:38:43 -0500

    Iniciar proyecto

---
+++ src/test/java/com/PublicarArticulos/GestorArticulos/GestorArticulosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PublicarArticulos.GestorArticulos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorArticulosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

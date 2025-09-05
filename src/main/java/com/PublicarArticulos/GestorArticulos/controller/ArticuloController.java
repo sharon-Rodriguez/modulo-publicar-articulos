586fad764b74f193b043ff2fbff5990d6770fea4
Sharon Rodriguez <sharo2599@gmail.com>
2025-09-05 10:53:04 -0500

    Configurar conexión de Spring Boot con MySQL en application.properties

--- src/main/java/com/PublicarArticulos/GestorArticulos/controller/ArticuloController.java
+++ src/main/java/com/PublicarArticulos/GestorArticulos/controller/ArticuloController.java
@@ -4,10 +4,53 @@
  */
 package com.PublicarArticulos.GestorArticulos.controller;
 
+import com.PublicarArticulos.GestorArticulos.model.Articulos;
+import com.PublicarArticulos.GestorArticulos.repository.ArticuloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  *
  * @author USER
  */
+@RestController
+@RequestMapping("/articulos")
 public class ArticuloController {
-    
+
+    @Autowired
+    private ArticuloRepository articuloRepository;
+
+    // CREATE
+    @PostMapping
+    public Articulos crearArticulo(@RequestBody Articulos articulo) {
+        return articuloRepository.save(articulo);
+    }
+
+    // READ - todos
+    @GetMapping
+    public List<Articulos> obtenerArticulos() {
+        return articuloRepository.findAll();
+    }
+
+    // READ - uno por id
+    @GetMapping("/{id}")
+    public Optional<Articulos> obtenerArticuloPorId(@PathVariable Integer id) {
+        return articuloRepository.findById(id);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public Articulos actualizarArticulo(@PathVariable Integer id, @RequestBody Articulos articulo) {
+        articulo.setIdArticulos(id); // asegurar que actualice el correcto
+        return articuloRepository.save(articulo);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void eliminarArticulo(@PathVariable Integer id) {
+        articuloRepository.deleteById(id);
+    }
 }
